f747451d04930dab56bb83cfc9987973d006d40d
josh <joshua@marinacci.org>
2022-01-07 13:08:32 -0800

    add input test

---
+++ input/src/main.rs
@@ -0,0 +1,13 @@
+// cli/"tui" shared between the evtest examples
+mod _pick_device;
+
+fn main() {
+    let mut d = _pick_device::pick_device();
+    println!("{}", d);
+    println!("Events:");
+    loop {
+        for ev in d.fetch_events().unwrap() {
+            println!("{:?}", ev);
+        }
+    }
+}
