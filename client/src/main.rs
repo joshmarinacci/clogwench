63342c8acde9504a3632de5afe05c2f03671a5f5
josh <joshua@marinacci.org>
2022-01-08 12:45:15 -0800

    start using commandline opts

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,18 +1,43 @@
 use interprocess::local_socket::LocalSocketStream;
 use std::io::{self, prelude::*, BufReader};
-use std::thread;
+use std::net::TcpStream;
+use std::{env, thread};
 use std::time::Duration;
 use common::{APICommand, ARGBColor, DrawRectCommand, OpenWindowCommand};
 use serde::{Deserialize, Serialize};
 use rand::prelude::*;
 
 
+
 fn main()  {
-    thread::sleep(Duration::from_millis(1000));
+    let args:Vec<String> = env::args().collect();
+    println!("args {:?}",args);
+    let delay = &args[1];
+    println!("delay is {}",delay);
     let mut rng = rand::thread_rng();
-    let mut conn = BufReader::new(
-        LocalSocketStream::connect("/tmp/teletype.sock").expect("failed to connect"),
-    );
+    match TcpStream::connect("localhost:3333") {
+        Ok(mut stream) => {
+            println!("connected to the server");
+            for i in 0..5 {
+                let cmd = APICommand::OpenWindowCommand(OpenWindowCommand{
+                    name: rng.gen_range(0..5)
+                });
+                let data = serde_json::to_string(&cmd).unwrap();
+                println!("sending data {:?}",data);
+                stream.write_all(data.as_ref()).expect("failed to send rect");
+                thread::sleep(Duration::from_millis(1000));
+            }
+        },
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
+    println!("Terminated.");
+/*
+
+    // let mut conn = BufReader::new(
+    //     LocalSocketStream::connect("/tmp/teletype.sock").expect("failed to connect"),
+    // );
     eprintln!("Teletype client connected to server.");
     let mut our_turn = true;
     let mut buffer = String::new();
@@ -45,4 +70,6 @@ fn main()  {
         buffer.clear();
         our_turn = !our_turn;
     }
+
+ */
 }
--- test-server/src/main.rs
+++ test-server/src/main.rs
@@ -1,19 +1,32 @@
 use std::io::Read;
 use std::net::{TcpListener, TcpStream};
-use socket2::{Domain, SockAddr, Socket, Type};
-
+use serde::Deserialize;
+use serde_json;
 use std::sync::{Arc, mpsc};
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
-use common::{APICommand, ARGBColor, KeyDownEvent, MouseMoveEvent};
+use common::{APICommand, };
 use ctrlc;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "example", about = "An example of StructOpt usage.")]
+struct Cli {
+    #[structopt(short, long)]
+    debug:bool,
+    #[structopt(short, long, default_value="10")]
+    timeout:u32,
+}
+
 
 fn main() {
-    let should_stop:Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    let args:Cli = Cli::from_args();
+    println!("running with args {:?}",args);
 
+    let should_stop:Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
     setup_c_handler(should_stop.clone());
 
     let (tx, rx) = mpsc::channel::<APICommand>();
@@ -21,28 +34,39 @@ fn main() {
     //setup network listener
     setup_network_listener(should_stop.clone(), tx);
 
+    start_event_processor(should_stop.clone(),rx);
     //start a child process
     // let ch = start_process();
 
 
-    let timeout = start_timeout(should_stop.clone());
+    let timeout = start_timeout(should_stop.clone(),args.timeout);
     timeout.join().unwrap();
     println!("all done now");
 
 }
 
+fn start_event_processor(stop: Arc<AtomicBool>, rx: Receiver<APICommand>) -> JoinHandle<()> {
+    return thread::spawn(move || {
+        for cmd in rx {
+            println!("server processing event {:?}",cmd);
+        }
+    });
+}
+
 fn setup_network_listener(stop: Arc<AtomicBool>, tx: Sender<APICommand>) -> JoinHandle<()> {
     return thread::spawn(move || {
         println!("starting network connection");
-        let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
-        println!("server listening on port 3333");
+        let port = 3333;
+        let listener = TcpListener::bind(format!("0.0.0.0:{}",port)).unwrap();
+        println!("server listening on port {}",port);
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
                     println!("got a new connection");
+                    let txx = tx.clone();
                     thread::spawn(move||{
-                        handle_client(stream);
-                    })
+                        handle_client(stream,txx);
+                    });
                 }
                 Err(e) => {
                     println!("error: {}",e);
@@ -54,31 +78,42 @@ fn setup_network_listener(stop: Arc<AtomicBool>, tx: Sender<APICommand>) -> Join
 }
 // adapated from
 // https://riptutorial.com/rust/example/4404/a-simple-tcp-client-and-server-application--echo
-fn handle_client(mut stream: TcpStream) {
-    let mut data = [0 as u8; 50];
-    while match stream.read(&mut data) {
-        Ok(size) => {
-            println!("got a message");
-            true
-        },
-        Err(_) => {
-            println!("");
-            false
-        }
-    } {}
+fn handle_client(mut stream: TcpStream, tx: Sender<APICommand>) {
+    let mut de = serde_json::Deserializer::from_reader(stream);
+    loop {
+        // if arc.load(Ordering::Relaxed) == true {
+        //     println!("socket thread stopping");
+        //     break;
+        // }
+        println!("server reading from socket");
+        let cmd:APICommand =APICommand::deserialize(&mut de).unwrap();
+        println!("server is getting results {:?}",cmd);
+        tx.send(cmd).unwrap();
+    }
+    // let mut data = [0 as u8; 500];
+    // while match stream.read_to_end(&mut data) {
+    //     Ok(size) => {
+    //         println!("got a message");
+    //         true
+    //     },
+    //     Err(_) => {
+    //         println!("");
+    //         false
+    //     }
+    // } {}
 }
 
-fn start_timeout(stop: Arc<AtomicBool>) -> JoinHandle<()> {
+fn start_timeout(stop: Arc<AtomicBool>, max_seconds:u32) -> JoinHandle<()> {
     return thread::spawn(move || {
-        println!("timeout will end in 15 seconds");
+        println!("timeout will end in {} seconds",max_seconds);
         let mut count = 0;
         loop {
             count = count + 1;
-            if count > 15 {
+            if count > max_seconds {
                 stop.store(true,Ordering::Relaxed);
             }
-            // println!("watchdog sleeping for 1000");
-            thread::sleep(Duration::from_millis(100));
+            // println!("watchdog sleeping for 1000 {}",count);
+            thread::sleep(Duration::from_millis(1000));
 
             if stop.load(Ordering::Relaxed) == true {
                 // println!("render thread stopping");
