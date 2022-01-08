8ac2706877cbbe496afe05926e5643b0adb3d4f7
josh <joshua@marinacci.org>
2022-01-08 13:15:28 -0800

    big refactor to use proper logging and command line options

--- client/src/main.rs
+++ client/src/main.rs
@@ -33,43 +33,4 @@ fn main()  {
         }
     }
     println!("Terminated.");
-/*
-
-    // let mut conn = BufReader::new(
-    //     LocalSocketStream::connect("/tmp/teletype.sock").expect("failed to connect"),
-    // );
-    eprintln!("Teletype client connected to server.");
-    let mut our_turn = true;
-    let mut buffer = String::new();
-    let mut count = 0;
-    loop {
-        count = count + 1;
-        let cmd = if count % 2 == 0 {
-            APICommand::DrawRectCommand(DrawRectCommand{
-                x: rng.gen_range(0..100),
-                y: rng.gen_range(0..100),
-                w: 100,
-                h: 100,
-                color: ARGBColor {
-                    r: rng.gen_range(0..255),
-                    g: rng.gen_range(0..255),
-                    b: rng.gen_range(0..255),
-                    a: 255
-                }
-            })
-        } else {
-            APICommand::OpenWindowCommand(OpenWindowCommand{
-                name: 0
-            })
-        };
-        let data = serde_json::to_string(&cmd).unwrap();
-        conn.get_mut().write_all(data.as_ref()).expect("failed to send rect");
-        // conn.get_mut().write(b"\n");
-        println!("sent data {:?}",data);
-        thread::sleep(Duration::from_millis(1000));
-        buffer.clear();
-        our_turn = !our_turn;
-    }
-
- */
 }
--- test-server/src/main.rs
+++ test-server/src/main.rs
@@ -11,121 +11,119 @@ use std::time::Duration;
 use common::{APICommand, };
 use ctrlc;
 use structopt::StructOpt;
-
-#[derive(StructOpt, Debug)]
-#[structopt(name = "example", about = "An example of StructOpt usage.")]
-struct Cli {
-    #[structopt(short, long)]
-    debug:bool,
-    #[structopt(short, long, default_value="10")]
-    timeout:u32,
-}
-
+use log::{info, warn, error,log};
+use env_logger;
+use env_logger::Env;
+use serde_json::Error;
 
 fn main() {
-    let args:Cli = Cli::from_args();
-    println!("running with args {:?}",args);
+    let args:Cli = init_setup();
 
-    let should_stop:Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
-    setup_c_handler(should_stop.clone());
+    let stop:Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    setup_c_handler(stop.clone());
 
     let (tx, rx) = mpsc::channel::<APICommand>();
 
-    //setup network listener
-    setup_network_listener(should_stop.clone(), tx);
-
-    start_event_processor(should_stop.clone(),rx);
-    //start a child process
+    start_network_server(stop.clone(), tx);
+    start_event_processor(stop.clone(), rx);
     // let ch = start_process();
+    let timeout_handle = start_timeout(stop.clone(), args.timeout);
+    timeout_handle.join().unwrap();
+    info!("all done now");
+}
 
-
-    let timeout = start_timeout(should_stop.clone(),args.timeout);
-    timeout.join().unwrap();
-    println!("all done now");
-
+fn init_setup() -> Cli {
+    let args:Cli = Cli::from_args();
+    let loglevel = if args.debug { "debug"} else { "error"};
+    env_logger::Builder::from_env(Env::default().default_filter_or(loglevel)).init();
+    info!("running with args {:?}",args);
+    return args;
 }
 
 fn start_event_processor(stop: Arc<AtomicBool>, rx: Receiver<APICommand>) -> JoinHandle<()> {
     return thread::spawn(move || {
         for cmd in rx {
-            println!("server processing event {:?}",cmd);
+            info!("processing event {:?}",cmd);
         }
     });
 }
 
-fn setup_network_listener(stop: Arc<AtomicBool>, tx: Sender<APICommand>) -> JoinHandle<()> {
+fn start_network_server(stop: Arc<AtomicBool>, tx: Sender<APICommand>) -> JoinHandle<()> {
     return thread::spawn(move || {
-        println!("starting network connection");
+        info!("starting network connection");
         let port = 3333;
         let listener = TcpListener::bind(format!("0.0.0.0:{}",port)).unwrap();
-        println!("server listening on port {}",port);
+        info!("server listening on port {}",port);
         for stream in listener.incoming() {
+            if stop.load(Ordering::Relaxed) {
+                break;
+            }
             match stream {
                 Ok(stream) => {
-                    println!("got a new connection");
+                    info!("got a new connection");
                     let txx = tx.clone();
-                    thread::spawn(move||{
-                        handle_client(stream,txx);
-                    });
+                    let stop2 = stop.clone();
+                    thread::spawn(move||handle_client(stream,txx,stop2));
                 }
                 Err(e) => {
-                    println!("error: {}",e);
+                    error!("error: {}",e);
                 }
             }
         }
         drop(listener);
     })
 }
-// adapated from
-// https://riptutorial.com/rust/example/4404/a-simple-tcp-client-and-server-application--echo
-fn handle_client(mut stream: TcpStream, tx: Sender<APICommand>) {
+
+fn handle_client(mut stream: TcpStream, tx: Sender<APICommand>, stop: Arc<AtomicBool>) {
     let mut de = serde_json::Deserializer::from_reader(stream);
     loop {
-        // if arc.load(Ordering::Relaxed) == true {
-        //     println!("socket thread stopping");
-        //     break;
-        // }
-        println!("server reading from socket");
-        let cmd:APICommand =APICommand::deserialize(&mut de).unwrap();
-        println!("server is getting results {:?}",cmd);
-        tx.send(cmd).unwrap();
+        if stop.load(Ordering::Relaxed) == true {
+            info!("client thread stopping");
+            break;
+        }
+        match APICommand::deserialize(&mut de) {
+            Ok(cmd) => {
+                info!("server received command {:?}",cmd);
+                tx.send(cmd).unwrap();
+            }
+            Err(e) => {
+                error!("error deserializing from client {:?}",e);
+                break;
+            }
+        }
     }
-    // let mut data = [0 as u8; 500];
-    // while match stream.read_to_end(&mut data) {
-    //     Ok(size) => {
-    //         println!("got a message");
-    //         true
-    //     },
-    //     Err(_) => {
-    //         println!("");
-    //         false
-    //     }
-    // } {}
 }
 
 fn start_timeout(stop: Arc<AtomicBool>, max_seconds:u32) -> JoinHandle<()> {
     return thread::spawn(move || {
-        println!("timeout will end in {} seconds",max_seconds);
+        info!("timeout will end in {} seconds",max_seconds);
         let mut count = 0;
         loop {
             count = count + 1;
             if count > max_seconds {
+                info!("timeout triggered");
                 stop.store(true,Ordering::Relaxed);
             }
-            // println!("watchdog sleeping for 1000 {}",count);
             thread::sleep(Duration::from_millis(1000));
-
-            if stop.load(Ordering::Relaxed) == true {
-                // println!("render thread stopping");
-                break;
-            }
+            if stop.load(Ordering::Relaxed) == true { break; }
         }
     });
-
 }
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "example", about = "An example of StructOpt usage.")]
+struct Cli {
+    #[structopt(short, long)]
+    debug:bool,
+    #[structopt(short, long, default_value="60")]
+    timeout:u32,
+}
+
+
 
 fn setup_c_handler(stop: Arc<AtomicBool>) {
     ctrlc::set_handler(move || {
+        error!("control C pressed. stopping everything");
         stop.store(true, Ordering::Relaxed)
     }).expect("error setting control C handler");
 }
