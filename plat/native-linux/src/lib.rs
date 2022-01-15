a8dded1cf72e24a8ae38a0e81a3c20ccbf7f0c8e
Josh Marinacci <joshua@marinacci.org>
2022-01-15 15:58:22 -0800

    start on linux native port

--- plat/native-linux/src/lib.rs
+++ plat/native-linux/src/lib.rs
@@ -1,12 +1,35 @@
+use std::sync::mpsc::Sender;
+use common::{ARGBColor, IncomingMessage, Rect};
+use common::graphics::GFXBuffer;
+
 pub struct Plat {
 
 }
 
-impl Plat {
-    pub fn init() -> Plat {
-        Plat {
+pub fn make_plat<'a>(sender: Sender<IncomingMessage>) -> Result<Plat, String> {
+    return Ok(Plat {
+        // sender: sender,
+    });
+}
 
+impl Plat {
+    pub fn get_screen_bounds(&self) -> Rect {
+        Rect {
+            x: 0,
+            y: 0,
+            w: 0,
+            h: 0
         }
+    }
+    pub fn service_input(&mut self) {}
+    pub fn service_loop(&mut self) {}
+    pub fn fill_rect(&mut self, rect: Rect, color: &ARGBColor) {}
+    pub fn draw_rect(&mut self, rect: Rect, color: &ARGBColor, width: i32) {}
+    pub fn draw_image(&mut self, x: i32, y: i32, img: &GFXBuffer) {}
+    pub fn clear(&mut self) {}
+    pub fn shutdown(&mut self) {}
+    pub fn register_image2(&mut self) {
+
     }
 }
 
