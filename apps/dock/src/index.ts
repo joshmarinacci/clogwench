9699a441158bb53dec1535cebebf2e9e41e1bca8
Josh Marinacci <joshua@marinacci>
2022-06-25 19:34:09 -0700

    make the dock really open the music app

--- apps/dock/src/index.ts
+++ apps/dock/src/index.ts
@@ -5,28 +5,36 @@ import {
 } from "thneed-gfx";
 import {App} from "../../common/src/app";
 import {ClogwenchWindowSurface} from "../../common/src/surface";
+import * as child_process from "child_process";
 
 
 function start(surface: ClogwenchWindowSurface) {
-    let button = new ActionButton()
-    button.set_caption("quit")
-    button.on(COMMAND_ACTION, async () => {
-        process.exit(0)
-    })
-
     let vbox = new VBox()
-    vbox.add(button)
+
     let label = new Label()
-    label.set_caption('text label')
+    label.set_caption('Dock')
     vbox.add(label)
 
-    let button2 = new ActionButton()
-    button2.set_caption('action button')
-    vbox.add(button2)
+    let music_button = new ActionButton()
+    music_button.set_caption('Music')
+    music_button.on(COMMAND_ACTION, () => {
+        console.log("launching the music app")
+        child_process.spawn('npm',['run','dev-node'],{
+            cwd:'../musicplayer/',
+            detached:true,
+        })
+    })
+    vbox.add(music_button)
 
-    let text = new TextLine()
-    text.set_text('some text to edit')
-    vbox.add(text)
+
+
+    let quit_button = new ActionButton()
+    quit_button.set_caption("quit")
+    quit_button.on(COMMAND_ACTION, async () => {
+        process.exit(0)
+    })
+    vbox.add(quit_button)
+
 
     let root = new LayerView('root-layer')
     root.add(vbox)
@@ -38,7 +46,7 @@ async function doit() {
     let app = new App()
     await app.connect()
     await app.send_and_wait({AppConnect: {HelloApp: {}}})
-    let win = await app.open_window(new Rect(50, 50, 300, 300))
+    let win = await app.open_window(new Rect(50, 50, 100, 300))
     let surface = new ClogwenchWindowSurface(win);
     start(surface)
 }
