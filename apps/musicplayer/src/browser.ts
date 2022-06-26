c83664d983512563b8cc8cb07d1a85b12e8d250b
Josh Marinacci <joshua@marinacci>
2022-06-25 19:11:26 -0700

    use input repainting

--- apps/musicplayer/src/browser.ts
+++ apps/musicplayer/src/browser.ts
@@ -8,7 +8,7 @@ export function start_browser() {
     let surface = new CanvasSurface(600,300, 1.0);
     let music_root:View = make_music_player(surface) as View;
     surface.set_root(music_root)
-    surface.start()
+    surface.start_input()
     surface.repaint()
 }
 
